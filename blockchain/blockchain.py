4f0b5dba93c4f26a0ea2ba6ece63d41939e2ad92
Jasmeet1709 <jsmtbagga45@gmail.com>
2018-07-22 13:18:12 +0530

    genesis block added

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -7,3 +7,13 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.createBlock(proof = 1, previous_hash = '0')
+
+    def createBlock(self, proof, previous_hash):
+        block = {
+            'index' : len(self.chain)+1,
+            'timestamp' : str(datetime.datetime.now()),
+            'proof' : proof,
+            'previous_hash' : previous_hash
+        }
+        self.chain.append(block)
+        return block
