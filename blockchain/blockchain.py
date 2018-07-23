987cd6044411ed7eacaf49aa3a2dac7e566164bb
Jasmeet1709 <jsmtbagga45@gmail.com>
2018-07-23 16:01:24 +0530

    getting a full chain route added

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -76,3 +76,12 @@ def mine_block():
         'previous_hash' : block['previous_hash']
     }
     return jsonify(response), 200
+
+# getting a full chain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain)
+    }
+    return jsonify(response), 200
